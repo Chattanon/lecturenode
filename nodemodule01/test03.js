c2d8ae79147e54d4154f1c3632f1a79afa00b44c
Chattanon <s6419410039@sau.ac.th>
2025-03-04 10:45:41 +0700

    V2

---
+++ nodemodule01/test03.js
@@ -0,0 +1,14 @@
+// common js
+const {sayHello, sayHi} = require('./iotwow.js')
+const {sumNumber} = require('./iotwoo.js')
+const iotwee = require('./iotwee.js')
+console.log(sayHello("Chattanon"))
+console.log(sayHi("mom"))
+
+console.log(sumNumber(50,55))
+
+console.log(iotwee.data3)
+
+console.log(iotwee.sayHey('somsuk'))
+
+
