67e1853b5c96aae667701f8d28c7b917e8173a7d
Chattanon <s6419410039@sau.ac.th>
2025-03-11 10:58:33 +0700

    study npm and node module

---
+++ nodemodule03/test01.js
@@ -0,0 +1,5 @@
+// การใช้งานโมดูลแบบ esmodule แบบที่ 1
+import {dataA,sayHi,sawasdee} from './my-module-b.js'
+
+console.log(dataA)
+sawasdee('สมศรี')
---
+++ nodemodule04/test01.js
@@ -0,0 +1,9 @@
+// const {calSquareArea,calTriangleArea,calCircleArea} = require('nin-calculate-area')
+
+// console.log(`พท.สี่เหลี่ยมกว้าง 10 ยาว 20 คือ ${calSquareArea(10,20)}`) //พท.สี่เหลี่ยมกว้าง 10 ยาว 20 คือ 200calTriangleArea(10,20))
+
+import {calSquareArea,calTriangleArea,calCircleArea} from 'nin-calculate-area'
+
+console.log(`พท.สี่เหลี่ยมกว้าง 10 ยาว 20 คือ ${calSquareArea(10,20)}`) //พท.สี่เหลี่ยมกว้าง 10 ยาว 20 คือ 200calTriangleArea(10,20))
+console.log(`พท.วงกลมรัศมี 20 คือ ${calCircleArea(20)}`)
+console.log(`พท.สามเหลี่ยมกว้าง 10 ยาว 20 คือ ${calTriangleArea(10,20)}`)
